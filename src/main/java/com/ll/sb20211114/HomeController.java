c338d5ee41553698ac683d05650c3afdfb4de8ac
mklol00 <dlwodn97@naver.com>
2023-11-14 10:44:48 +0900

    calc

--- src/main/java/com/ll/sb20211114/HomeController.java
+++ src/main/java/com/ll/sb20211114/HomeController.java
@@ -8,13 +8,28 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HomeController {
 
     @GetMapping("/")
-    @ResponseBody // 이 함수의 리턴값을 그대로 브라우저에 전송해
-    String showMain(){
+    @ResponseBody
+        // 이 함수의 리턴값을 그대로 브라우저에 전송하라는 의미
+    String showMain() {
+        System.out.println("안녕하세요!!!"); // 이 메시지는 콘솔에 출력됨, 클라이언트에게 전송되지 않습니다.
         return "안녕하세요.";
     }
+
     @GetMapping("/about")
     @ResponseBody
-    String showAbout(){
-        return "개발자커뮤니티";
+    String showAbout() {
+        return "개발자 커뮤니티";
+    }
+
+    @GetMapping("/calc")
+    @ResponseBody
+    String showCalc(int a, int b) {
+        return "계산 결과 : %d".formatted(a + b);
+    }
+
+    @GetMapping("/calc2")
+    @ResponseBody
+    String showCalc2(Integer a, Integer b) {
+        return "a : " + a + ", b : " + b;
     }
 }
