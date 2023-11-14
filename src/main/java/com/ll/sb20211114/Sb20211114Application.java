91a5030edb1faba1083a4c2b815ede8c839cc60c
mklol00 <dlwodn97@naver.com>
2023-11-14 09:59:54 +0900

    init

---
+++ src/main/java/com/ll/sb20211114/Sb20211114Application.java
@@ -0,0 +1,13 @@
+package com.ll.sb20211114;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb20211114Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb20211114Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/sb20211114/Sb20211114ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.sb20211114;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb20211114ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
